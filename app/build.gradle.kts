bd3b04d4cef588813e4c5ee26a42b740cbfa6e5d
anujrathore122508 <anuj92089@gmail.com>
2024-04-20 20:00:56 +0530

    icon add login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+        // ...
+
+        // Import the Firebase BoM
+        implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+
+        // When using the BoM, you don't specify versions in Firebase library dependencies
+
+        // Add the dependency for the Firebase SDK for Google Analytics
+        implementation("com.google.firebase:firebase-analytics")
+
+        // TODO: Add the dependencies for any other Firebase products you want to use
+        // See https://firebase.google.com/docs/android/setup#available-libraries
+        // For example, add the dependencies for Firebase Authentication and Cloud Firestore
+        implementation("com.google.firebase:firebase-auth")
+        implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
+
 }
